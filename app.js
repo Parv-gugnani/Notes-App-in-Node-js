58cf75422742fc858c706ce7cd91e932757504fa
Parv-gugnani <9765.parv@gmail.com>
2023-09-29 16:27:35 +0530

    require

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const fs = require("fs");
+require("./utils.js");
+// const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "This file was created by Parv.js");
-fs.appendFileSync("notes.txt", "\nhello");
+// fs.writeFileSync("notes.txt", "This file was created by Parv.js");
+// fs.appendFileSync("notes.txt", "\nhello");
+
+const name = "Parv";
+console.log(name);
