6da3123418054e05759bb9711af578cb8ae06b56
Parv-gugnani <9765.parv@gmail.com>
2023-10-01 14:01:02 +0530

    adding

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const add = require("./utils.js");
+// const add = require("./utils.js");
+const getNotes = require("./notes.js");
 // const fs = require("fs");
 
 // fs.writeFileSync("notes.txt", "This file was created by Parv.js");
 // fs.appendFileSync("notes.txt", "\nhello");
-const sum = add(4, 2);
-console.log(sum);
+// const sum = add(4, 2);
+const msg = getNotes();
+console.log(msg);
+// console.log(sum);
