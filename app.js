8ace6392afef575cb5c356abfd57b4bee420a67d
Parv-gugnani <9765.parv@gmail.com>
2023-10-01 13:14:39 +0530

    node

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-require("./utils.js");
+const name = require("./utils.js");
 // const fs = require("fs");
 
 // fs.writeFileSync("notes.txt", "This file was created by Parv.js");
 // fs.appendFileSync("notes.txt", "\nhello");
 
-const name = "Parv";
 console.log(name);
--- utils.js
+++ utils.js
@@ -1,1 +1,10 @@
 console.log("utils.js");
+
+const name = "Parv";
+
+const add = function (a, b) {
+  return a + b;
+};
+
+module.exports = add;
+// exporting the thing so that i can use it where i requrie this file
