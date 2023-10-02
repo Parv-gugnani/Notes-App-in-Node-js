457169a11b504084f53635aa24e3417b28bafefb
Parv-gugnani <9765.parv@gmail.com>
2023-10-02 18:19:26 +0530

    valid

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // const add = require("./utils.js");
+const validator = require("validator");
 const getNotes = require("./notes.js");
 // const fs = require("fs");
 
 // fs.writeFileSync("notes.txt", "This file was created by Parv.js");
 // fs.appendFileSync("notes.txt", "\nhello");
 // const sum = add(4, 2);
-const msg = getNotes();
-console.log(msg);
+// const msg = getNotes();
+// console.log(msg);
 // console.log(sum);
+
+// checking if it is email or not
+console.log(validator.isEmail("parv@example.com"));
+
+//check url
+console.log(validator.isURL("example@"));
