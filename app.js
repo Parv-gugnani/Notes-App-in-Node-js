4a76109e50b1b6e8f5329a93d45ac33b70b34dc9
Parv-gugnani <9765.parv@gmail.com>
2023-10-04 15:02:46 +0530

    remove notes

--- app.js
+++ app.js
@@ -63,8 +63,11 @@ yargs.command({
 yargs.command({
   command: "remove",
   describe: "Remove a note",
-  handler: function () {
-    console.log("Removing a new note");
+  builder: {
+    title: { describe: "Note title", demandOption: true, type: "string" },
+  },
+  handler: function (argv) {
+    notes.removeNotes(argv.title);
   },
 });
 
--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const chalk = require("chalk");
 
 const getNotes = function () {
   return "Your Notes...";
@@ -18,10 +19,30 @@ const addNotes = function (title, body) {
     saveNotes(notes);
     console.log("new notes added");
   } else {
-    console.log("Note is already added");
+    console.log(chalk.blue.inverse("Note is already added"));
   }
   // adding // checking if it is already in the notes
 };
+
+//removing notes
+const removeNotes = function (title) {
+  const notes = loadNotes();
+
+  const updatedNotes = notes.filter(function (note) {
+    return note.title !== title;
+  });
+
+  if (notes.length === updatedNotes.length) {
+    console.log(chalk.red.inverse("Not Found!"));
+  } else {
+    console.log(chalk.green.inverse("Removed!"));
+    saveNotes(updatedNotes);
+  }
+};
+
+//
+
+// saving
 
 const saveNotes = function (notes) {
   const dataJson = JSON.stringify(notes);
@@ -41,4 +62,5 @@ const loadNotes = function () {
 module.exports = {
   getNotes: getNotes,
   addNotes: addNotes,
+  removeNotes: removeNotes,
 };
